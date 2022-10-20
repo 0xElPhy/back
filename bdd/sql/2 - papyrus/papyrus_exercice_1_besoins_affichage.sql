13a0febaccdc4da7d9651154069aced7cf8df4f2
0xElPhy <github@0xmartel.com>
2022-10-20 16:58:03 +0200

    modifs du jour bonjour

--- bdd/sql/2 - papyrus/papyrus_exercice_1_besoins_affichage.sql
+++ bdd/sql/2 - papyrus/papyrus_exercice_1_besoins_affichage.sql
@@ -31,7 +31,7 @@ WHERE (posfou LIKE '75%'
     OR posfou LIKE '77%'
     OR posfou LIKE '78%'
     OR posfou LIKE '92%')
-ORDER BY 2 ASC;
+ORDER BY 1 DESC, nomfou;
 
 -- 6 - Quelles sont les commandes passées en mars et en avril ?
 SELECT
@@ -44,7 +44,7 @@ WHERE (MONTH(datcom) = 3
 -- 7 - Quelles sont les commandes du jour qui ont des observations particulières ?
 SELECT numcom, datcom
 FROM entcom
-Where obscom <> '';
+Where obscom NOT LIKE '';
 -- ATTENTION, on en peut pas utiliser
 -- 'WHERE obscmon IS NOT NULL'
 -- car le type de data dans la colonne est défini en VARCHAR !!!!!
@@ -53,7 +53,7 @@ Where obscom <> '';
 SELECT numcom, SUM(qtecde * priuni) AS 'Prix total commande'
 FROM ligcom
 GROUP BY numcom
-ORDER BY 2 DESC
+ORDER BY 2 DESC;
 
 -- 9 - Lister les commandes dont le total est supérieur à 10000€ ; on exclura dans le calcul du total les articles commandés en quantité supérieure ou égale à 1000.
 SELECT
@@ -81,35 +81,87 @@ FROM fournis
 JOIN entcom ON entcom.numfou = fournis.numfou
 JOIN ligcom ON ligcom.numcom = entcom.numcom
 JOIN produit ON produit.codart = ligcom.codart
-WHERE RIGHT(entcom.obscom, 7) LIKE 'urgente'
+WHERE entcom.obscom LIKE '%urgent%'
 GROUP BY produit.libart
 ORDER BY 4 DESC;
 
 -- 12 - Coder de 2 manières différentes la requête suivante : Lister le nom des fournisseurs susceptibles de livrer au moins un article.
-
 -- Première façon
-SELECT
+SELECT DISTINCT fournis.nomfou AS 'Fournisseurs suceptibles de livrer au moins un article'
+FROM fournis
+JOIN entcom ON entcom.numfou = fournis.numfou
+JOIN ligcom ON ligcom.numcom = entcom.numcom
+WHERE ligcom.qteliv >= 1;
 
 -- Seconde façon
-SELECT
 
 -- 13 - Coder de 2 manières différentes la requête suivante : Lister les commandes dont le fournisseur est celui de la commande n°70210.
 -- Première façon
-SELECT numcom, datecom
+SELECT numcom AS 'Commandes du fournisseur de la commande 70210', datcom
 FROM entcom
 WHERE numfou = (
 	-- Sous-requête : Recupérer le numéro fournisseur de la commande n°70210
 	SELECT numfou
 	FROM entcom
-	WHERE numfou = 70210
+	WHERE numcom = 70210
 );
 
 -- Seconde façon
-SELECT numcom, datecom
+SELECT numcom AS 'Commandes du fournisseur de la commande 70210', datcom
 FROM entcom
-WHERE numfou = 120
+WHERE numfou = 120;
 
 -- 14 - Dans les articles susceptibles d’être vendus, lister les articles moins chers (basés sur Prix1) que le moins cher des rubans (article dont le premier caractère commence par R).
-SELECT libart, prix1
-FROM produit
+SELECT DISTINCT produit.libart , vente.prix1
+FROM vente
+JOIN produit ON produit.codart = vente.codart
+WHERE vente.prix1 < (
+	-- Sous-requête : Récupérer le moins cher des articles de type 'ruban'
+	SELECT MIN(vente.prix1)
+	FROM vente
+	JOIN produit ON produit.codart = vente.codart
+	WHERE produit.libart LIKE 'r%'
+)
+ORDER BY vente.prix1 DESC;
 
+-- 15 - Sortir la liste des fournisseurs susceptibles de livrer les produits dont le stock est inférieur ou égal à 150 % du stock d'alerte.
+SELECT fournis.nomfou, produit.libart
+FROM fournis
+JOIN vente ON vente.numfou = fournis.numfou
+JOIN produit ON produit.codart = vente.codart
+WHERE produit.stkphy <= produit.stkale * 150 / 100
+ORDER BY produit.libart, fournis.nomfou;
+
+-- 16 - Sortir la liste des fournisseurs susceptibles de livrer les produits dont le stock est inférieur ou égal à 150 % du stock d'alerte, et un délai de livraison d'au maximum 30 jours.
+SELECT fournis.nomfou, produit.libart
+FROM fournis
+JOIN vente ON vente.numfou = fournis.numfou
+JOIN produit ON produit.codart = vente.codart
+WHERE produit.stkphy <= produit.stkale * 150 / 100 AND vente.delliv <= 30
+ORDER BY fournis.nomfou, produit.libart;
+
+-- 17 - Avec le même type de sélection que ci-dessus, sortir un total des stocks par fournisseur, triés par total décroissant.
+SELECT fournis.nomfou, produit.libart, produit.stkphy
+FROM fournis
+JOIN vente ON vente.numfou = fournis.numfou
+JOIN produit ON produit.codart = vente.codart
+ORDER BY produit.stkphy DESC, produit.libart, fournis.nomfou;
+
+-- 18 - En fin d'année, sortir la liste des produits dont la quantité réellement commandée dépasse 90% de la quantité annuelle prévue.
+SELECT produit.libart
+FROM fournis
+JOIN vente ON vente.numfou = fournis.numfou
+JOIN produit ON produit.codart = vente.codart
+JOIN ligcom ON ligcom.codart = produit.codart
+WHERE ligcom.qtecde > produit.qteann * 90 / 100;
+
+-- 19 - Calculer le chiffre d'affaire par fournisseur pour l'année 2018, sachant que les prix indiqués sont hors taxes et que le taux de TVA est 20%.
+SELECT
+fournis.nomfou,
+SUM(ligcom.qtecde * ligcom.priuni) * 1.20 AS 'Chiffre d_Affaire pour l_année 2018'
+FROM fournis
+JOIN entcom ON entcom.numfou = fournis.numfou
+JOIN ligcom ON ligcom.numcom = entcom.numcom
+WHERE YEAR(datcom) = 2018
+GROUP BY fournis.nomfou
+ORDER BY 2 DESC;
---
+++ bdd/sql/3- evaluation/northwind_evaluation.sql
@@ -0,0 +1,63 @@
+-- Evaluation
+
+-- Requêtes de BDD
+
+-- 1 - Liste des clients français
+SELECT CompanyName, ContactName, ContactTitle, Phone
+FROM customers
+WHERE Country LIKE 'France';
+
+-- 2 - Liste des produits vendus par le fournisseur "Exotic Liquids"
+SELECT products.ProductName, products.UnitPrice
+FROM suppliers
+JOIN products ON products.SupplierID = suppliers.SupplierID
+WHERE suppliers.CompanyName LIKE 'Exotic Liquids%'
+ORDER BY products.UnitPrice DESC;
+
+-- 3 - Nombre de produits mis à disposition par les fournisseurs français (tri par nombre de produits décroissant) :
+SELECT suppliers.CompanyName, `order details`.Quantity
+FROM suppliers
+JOIN products ON products.SupplierID = suppliers.SupplierID
+JOIN `order details` ON `order details`.ProductID = products.ProductID
+WHERE suppliers.Country LIKE 'France';
+
+-- 3 - Nombre de produits mis à disposition par les fournisseurs français (tri par nombre de produits dé>
+SELECT suppliers.CompanyName, COUNT(*) AS 'Nombre de produits'
+FROM suppliers
+JOIN products ON products.SupplierID = suppliers.SupplierID 
+WHERE suppliers.Country LIKE 'France'
+GROUP BY suppliers.CompanyName
+ORDER BY 2 DESC;
+
+-- 4 - Liste des clients français ayant passé plus de 10 commandes :
+SELECT customers.CompanyName, COUNT(*) AS 'Nombre de commandes'
+FROM customers
+JOIN orders ON orders.CustomerID = customers.CustomerID
+WHERE customers.Country LIKE 'France'
+GROUP BY customers.CompanyName
+HAVING COUNT(*) > 10
+ORDER BY 2 DESC;
+
+-- 5 - Liste des clients dont le montant cumulé de toutes les commandes passées est supérieur à 30000 € :
+SELECT
+customers.CompanyName,
+ROUND(SUM(`order details`.UnitPrice * `order details`.Quantity),0) AS 'CA',
+customers.Country
+FROM customers
+JOIN orders ON orders.CustomerID = customers.CustomerID
+JOIN `order details` ON `order details`.OrderID = orders.OrderID
+GROUP BY customers.CompanyName
+HAVING CA > 30000
+ORDER BY 2 DESC;
+
+-- 6 - Liste des pays dans lesquels des produits fournis par "Exotic Liquids" ont été livrés :
+SELECT DISTINCT orders.ShipCountry
+FROM orders
+JOIN `order details` ON `order details`.OrderID = orders.OrderID
+JOIN products ON products.ProduitID = `order details`.ProductID
+JOIN suppliers ON suppliers.SupplierID = products.SupplierID
+WHERE suppliers.CompanyName LIKE 'Exotic Liquids%'
+ORDER BY 1;
+
+-- 7 - Chiffre d'affaires global sur les ventes de 1997 :
+SELECT 

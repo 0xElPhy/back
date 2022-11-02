f1c73cf06584081dbad442f24cf77f2d474dd73c
0xElPhy <github@0xmartel.com>
2022-11-02 16:40:41 +0100

    update

---
+++ serveur/php/7_bibliotheques/fonctions.php
@@ -0,0 +1,6 @@
+<?php
+    function writeMessage($text) {
+    $html = "<h1>".$text."</h1>";
+    echo $html;
+    }  
+?>
---
+++ serveur/php/PDO/artists.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO</title>
+</head>
+<body>
+    <?php
+    
+    // on importe le contenu du fichier "db.php"
+    include "db.php";
+    // on exécute la méthode de connexion à notre BDD
+    $db = connexionBase();
+
+    // on lance une requête pour chercher toutes les fiches d'artistes
+    $requete = $db->query("SELECT * FROM artist");
+    // on récupère tous les résultats trouvés dans une variable
+    $tableau = $requete->fetchAll(PDO::FETCH_OBJ);
+    // visualiser le contenu de $tableau
+    var_dump($tableau);
+    // on clôt la requête en BDD
+    $requete->closeCursor();
+
+    ?>
+    </body>
+</html>

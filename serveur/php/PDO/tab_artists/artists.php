d5dad2c296bd7c6d7e7174a2de5764c6d0bd43ad
0xElPhy <github@0xmartel.com>
2022-11-13 10:50:22 +0100

    css

--- serveur/php/PDO/tab_artists/artists.php
+++ serveur/php/PDO/tab_artists/artists.php
@@ -19,7 +19,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" media="screen" href="/back/serveur/php/PDO/assets/css/test.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="/back/serveur/php/PDO/assets/css/artists_cards.css">
     <link rel="stylesheet" type="text/css" media="screen" href="/back/serveur/php/PDO/assets/css/pages_content.css">
     <title>PDO - Liste</title>
 </head>
@@ -28,21 +28,43 @@
     <?php include('../structure/nav.php'); ?>
 
     <section class="container">
-        <h1>Liste des artistes (<?php echo "en cours" ?>)</h1>
-        <a href="artist_new.php"><button>Ajouter Artiste</button></a>
+        <section class="sub_container titre">
+            <h1>Liste des artistes (<?php echo "en cours" ?>)</h1>
+            <a href="artist_new.php"><button>Ajouter Artiste</button></a>
+        </section>
 
-        <?php foreach ($tableau as $artist): ?>
+        <section class="sub_container allCards">
+            <?php foreach ($tableau as $artist): ?>
+
+                <div class="card id_<?= $artist->artist_id ?>" onclick="location.href='artist_detail.php?id=<?= $artist->artist_id ?>'">
+                    <div class="border">
+                        <h2><?= $artist->artist_name ?></h2>
+                        <div class="infos">
+                            <i class="hover id">
+                                <?= (($artist->artist_id) < 10) ? "0".$artist->artist_id : $artist->artist_id; ?>
+                            </i>
+                            <a href="artist_detail.php?id=<?= $artist->artist_id ?>">
+                                <i class="hover details">Détails</i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach; ?>
+        </section>
+
+       <!-- </?php foreach ($tableau as $artist): ?>
             <div class="subcontainer">
                 <div class="card">
                         <h1 class="title">-</h1>
                         <div class="info">
-                            <h1><?= (($artist->artist_id) < 10) ? "0".$artist->artist_id : $artist->artist_id; ?></h1>
-                            <h2><?= $artist->artist_name ?></h2>
+                            <h1>/?= (($artist->artist_id) < 10) ? "0".$artist->artist_id : $artist->artist_id; ?></h1>
+                            <h2>/?= $artist->artist_name ?></h2>
                         </div>
-                        <a href="artist_detail.php?id=<?= $artist->artist_url ?>">Détail</a>
+                        <a href="artist_detail.php?id=</?= $artist->artist_id ?>">Détail</a>
                 </div>
             </div>
-        <?php endforeach; ?>
+        </?php endforeach; ?> -->!
 
         <!-- <table border=1>
             <tr>
